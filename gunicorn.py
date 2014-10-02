a0b61877c1b4d43e79d404387e509c4ae1779998
Atsuki Demizu <demmys.atsuki@gmail.com>
2014-10-02 23:39:30 +0900

    gunicornのデーモン化はSystemdで行うように変更

--- gunicorn.py
+++ gunicorn.py
@@ -1,6 +1,6 @@
 bind = '127.0.0.1:5000'
 workers = 4
 backlog = 2048
-pidfile = '/tmp/gunicorn.pid'
-logfile = 'log/gunicorn.log'
-loglevel = 'info'
+accesslog = '/var/log/gunicorn/access.log'
+errorlog = '/var/log/gunicorn/error.log'
+loglevel = 'debug'
