ee710ad8d6e918099d15698d6c2edab86d071f41
Atsuki Demizu <demmys.atsuki@gmail.com>
2014-10-07 19:06:12 +0900

    コマンド呼び出しをリファクタリング

--- service/lingr_bot.py
+++ service/lingr_bot.py
@@ -10,19 +10,23 @@ class CodeReadingBot(LingrBot):
         self._register('L\d+ *.+', self.comment_line)
 
     def code_reading_resolver(self, event):
+        print('[DEBUG] CODE READING COMMAND')
         args = re.split(' +', event.text())
-        command = args[1]
-        if command == 'start':
-            return self.start_code_reading(event)
-        elif command == 'end':
-            return self.end_code_reading(event)
+        f = {
+            'start': self.start_code_reading,
+            'end': self.end_code_reading
+        }[args[1]]
+        if f is not None:
+            return f(event)
         return ''
 
     def start_code_reading(self, event):
+        print('[DEBUG] START CODE READING')
         self.__reading_room[event.room()] = True
         return 'コード読書会 開始! @ ' + event.permalink()
 
     def end_code_reading(self, event):
+        print('[DEBUG] END CODE READING')
         if event.room() in self.__reading_room:
             del self.__reading_room[event.room()]
             return 'コード読書会 終了!'
@@ -30,6 +34,7 @@ class CodeReadingBot(LingrBot):
 
     def comment_line(self, event):
         if event.room() in self.__reading_room:
+            print('[DEBUG] COMMENT LINE')
             head = re.search('L\d+ *', event.text())
             comment = event.text()[head.end():]
             number = int(head.group()[1:])
